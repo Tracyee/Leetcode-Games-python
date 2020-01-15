d30a8df994be0a436ac0a7fc9e80a9e971f4eadc
Tracyee <caiyeee@outlook.com>
2020-01-15 20:08:15 +0800

    O(n) implementation

--- 1. Two Sum/my_solution.py
+++ 1. Two Sum/my_solution.py
@@ -1,21 +1,21 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        self.nums = nums
-        self.target = target
-        self.result = []
-    def sub(self):
-        for i in range(len(self.nums)):
-            result = self.target - self.nums[i]
-            if result in self.nums: # if true, answers found!
-                return [i, self.nums.index(result)]
+    def twoSum(self, nums, target):
+        buff = {}
+        for i in range(len(nums)):
+            result = target - nums[i]
+            if result in buff: # if true, answers found!
+                return [buff[result], i]
+            else:
+                buff[nums[i]] = i
+
 
 def main():
     # n = int(input("Please specify the number of elements: "))
     nums = list(map(int, input("Please tpye your choice of integers: ").split()))
     # nums.append(int(input("Please type ")))
     target = int(input("Please type your target as an integer: "))
-    sol = Solution.twoSum(nums, target)
-    print(sol.sub())
+    sol = Solution().twoSum(nums, target)
+    print(sol)
 
 if __name__ == "__main__":
     main()
