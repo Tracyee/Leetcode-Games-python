18302b13d7e854aab1cdc46777afd663587b3b03
Tracyee <caiyeee@outlook.com>
2020-01-17 15:29:28 +0800

    in test

--- 88. Merge Sorted Array/test.py
+++ 88. Merge Sorted Array/test.py
@@ -1,5 +1,5 @@
 from my_solution import Solution as sol1
-from greedy import Solution as sol2
+from twopointers_backwds import Solution as sol2
 
 
 def main():
@@ -7,7 +7,7 @@ def main():
     nums2 = list(map(int, input("Please tpye your choice of integers for nums2: ").split()))
     m = int(input("Please choose your m as the number of elements initialized in nums1"))
     m = int(input("Please choose your n as the number of elements initialized in nums2"))
-    ret = Solution().merge(nums1, m, nums2, n)
+    ret = sol1().merge(nums1, m, nums2, n)
     # ret = sol2().merge(nums1, m, nums2, n)
     out = str(nums1);
     print(out)
